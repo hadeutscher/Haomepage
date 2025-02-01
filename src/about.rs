bcc8d58160d675d5760681f907154425432fc09c
Yuval Deutscher <yuval@deut.sh>
2025-02-01 18:09:24 +0200

    Update site

--- src/about.rs
+++ src/about.rs
@@ -4,31 +4,35 @@ use yew::prelude::*;
 pub fn footer() -> Html {
     html! {
         <div class={classes!("container", "content")}>
-            <p class={classes!("f1")}> { "Hey there." } </p>
-            <p class={classes!("f2")}> { "I'm Yuval, but most people call me Deutscher. " } </p>
+            <p class={classes!("f1")}> { "Hello. I'm Yuval." } </p>
+            <p class={classes!("f2")}> { "You might also know me as Deutscher. " } </p>
+            <p class={classes!("f3")}> { "I'm a software engineer." } </p>
             <p>
-                { "I'm a software engineer." } <br /> { "I like video games, operating systems, and programming languages (especially Rust, which this site is written in).\n" }
+                { "I like video games, operating systems, and programming languages — especially Rust, which I used to create this site.\n" }
                 <br />
-                { "I like freshly roasted coffee and craft beer. I also sometimes dance West Coast Swing.\n" }
+                { "I enjoy coffee and craft beer, and sometimes dance West Coast Swing.\n" }
                 <br />
-                { "I'm also interested in philosophy and religion, and study a little bit of Talmud."}
+                { "I'm also interested in philosophy and religion; I've studied a bit of Talmud and am a fan of " } <a href="https://en.wikipedia.org/wiki/Friedrich_Hayek"> { "Friedrich A. Hayek" } </a> { "'s work." }
                 <br />
-                { "I drew my logo on my own, partially by " }
-                <a href="https://github.com/hadeutscher/Haomepage/blob/master/HaLogo.svg?short_path=898e08c"> { "hand-written SVG" } </a>
-                { ". It's based on the Sharingan from the anime series Naruto." }
+                { "The logo you see here is my own creation, partially crafted by " } <a href="https://github.com/hadeutscher/Haomepage/blob/master/HaLogo.svg?short_path=898e08c"> { "hand-written SVG" } </a> { ", and it's inspired by the Sharingan from the anime series Naruto." }
             </p>
             <p>
                 { "You can find my code on " }
                 <a href="https://github.com/hadeutscher/"> {"GitHub"} </a>
                 {", follow me on " }
                 <a href="https://twitter.com/hadeutscher/"> {"Twitter"} </a>
-                { " or connect with me on " }
+                { ", or connect with me on " }
                 <a href="https://www.linkedin.com/in/hadeutscher/"> {"LinkedIn"} </a>
                 { "." }
                 <br />
-                { "You can also send me an e-mail to " }
-                <a href="mailto:yuval@deut.sh"> {"yuval@deut.sh"} </a>
+                { "You can also reach out to me via email at " }
+                <a href="mailto:yuval@deut.sh"> { "yuval@deut.sh" } </a>
                 { "."}
+            </p>
+            <p>
+                { "Here are the links to the projects I'm currently working on: " }
+                <a href="https://harail.deut.sh/"> { "HaRail" } </a>
+                { " (" } <a href="https://github.com/hadeutscher/RustyRail"> { "GitHub" } </a> { ")" }
             </p>
         </div>
     }
--- src/footer.rs
+++ src/footer.rs
@@ -6,8 +6,13 @@ pub fn footer() -> Html {
         <footer>
             <div class={classes!("container")}>
                 <span class={classes!("attribution")}>
-                    { "© 2022 Yuval Deutscher. Source code available on " }
-                    <a href="https://github.com/hadeutscher/Haomepage"> { "GitHub" } </a>
+                    { "© 2024 Yuval Deutscher. Source code available on " }
+                    <a href="https://github.com/hadeutscher/Haomepage"> { "GitHub" } </a> { ". " }
+                </span>
+                <span class={classes!("footer-icons")}>
+                    <a href="https://ready.chair6.net/?url=deut.sh">
+                        <img src="World_IPv6_launch_logo.svg" height=24 alt="IPv6 Ready" title="IPv6 Ready" />
+                    </a>
                 </span>
             </div>
         </footer>
